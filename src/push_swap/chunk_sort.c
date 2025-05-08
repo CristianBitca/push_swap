637bcdb531ba286f2001c1d994212ffd333e2e7e
Cristian Bitca <cbitca@c1r7s1.42london.com>
2025-05-08 14:40:48 +0100

    beta

--- src/push_swap/chunk_sort.c
+++ src/push_swap/chunk_sort.c
@@ -34,10 +34,8 @@ int	chunk_number(int size)
 		return (8);
 	else if (size <= 300)
 		return (10);
-	else if (size <= 400)
-		return (12);
 	else
-		return (ft_sqrt(size) / 2);
+		return ((ft_sqrt(size) / 2) - 1);
 }
 
 t_chunk	*init_chunk(t_stack_list *stack_list, t_chunk *chunk)
@@ -71,6 +69,64 @@ t_chunk	*init_chunk(t_stack_list *stack_list, t_chunk *chunk)
 	return (chunk);
 }
 
+int	short_way(t_stack *stack, int n)
+{
+	t_node *node1;
+	t_node *node2;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	node1 = stack->first;
+	node2 = stack->last;
+	while (node1->i != n)
+	{
+		node1 = node1->next;
+		i++;
+	}
+	while (node2->i != n)
+	{
+		node2 = node2->prev;
+		j++;
+	}
+	if (j < i)
+		return (1);
+	return (0);
+}
+
+int	chunk_way(t_stack *stack, int min, int max)
+{
+	t_node *node1;
+	t_node *node2;
+	int	i;
+	int	j;
+	int	count;
+
+	i = 0;
+	j = 0;
+	count = 0;
+	node1 = stack->first;
+	node2 = stack->last;
+	while (count != stack->size / 2)
+	{
+		if (node1->i >= min && node1->i < max)
+			i++;
+		node1 = node1->next;
+		count++;
+	}
+	while (count != stack->size)
+	{
+		if (node2->i >= min && node2->i < max)
+			j++;
+		node2 = node2->prev;
+		count++;
+	}
+	if (j < i)
+		return (1);
+	return (0);
+}
+
 void	sorting_chunk(t_stack_list *stack_list, t_chunk *chunk, int i)
 {
 	int	count;
@@ -83,32 +139,43 @@ void	sorting_chunk(t_stack_list *stack_list, t_chunk *chunk, int i)
 		if (n >= chunk->min && n < chunk->max)
 		{
 			pb(stack_list);
-			if (n <= chunk->mid)
-				rb(stack_list);
+			if (n >= chunk->mid)
+			{
+				if (stack_list->a->first->next->i >= chunk->min
+					&& stack_list->a->first->i < chunk->max)
+					rb(stack_list);
+				else
+					rr(stack_list);
+			}
 			count++;
 		}
 		else
 			ra(stack_list);
+
 	}
 }
 
 void	return_chunk(t_stack_list *stack_list, t_chunk *chunk, int i)
 {
 	int	max;
+	int	way;
 	int	count;
 
 	count = 0;
 	max = chunk->max - 1;
 	while (count < chunk->size[i])
 	{
+		way = short_way(stack_list->b, max);
 		if (stack_list->b->first->i == max)
 		{
 			pa(stack_list);
 			count++;
 			max--;
 		}
-		else
+		else if (way == 0)
 			rb(stack_list);
+		else
+			rrb(stack_list);
 	}
 }
 
@@ -118,6 +185,12 @@ void	next_chunk(t_chunk *chunk, int i)
 	i++;
 	chunk->max += chunk->size[i];
 	chunk->mid = (chunk->min + chunk->max) / 2;
+}
+
+void	prev_chunk(t_chunk *chunk, int i)
+{
+	chunk->min -= chunk->size[i];
+	chunk->max -= chunk->size[i];
 }
 
 void	chunk_sort(t_stack_list *stack_list)
@@ -131,9 +204,14 @@ void	chunk_sort(t_stack_list *stack_list)
 	while (i < chunk->number)
 	{
 		sorting_chunk(stack_list, chunk, i);
-		return_chunk(stack_list, chunk, i);
 		next_chunk(chunk, i);
 		i++;
+	}
+	while (stack_list->b->size)
+	{
+		return_chunk(stack_list, chunk, i);
+		prev_chunk(chunk, i);
+		i--;
 	}
 	free(chunk->size);
 	free(chunk);
